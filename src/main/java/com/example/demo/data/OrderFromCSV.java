44200ac0f5e8c58813ac6d4be1b1dea9f304ab82
PawelKuch <kucharski.pawel99@gmail.com>
2024-02-27 21:05:52 +0100

    sklepik

---
+++ src/main/java/com/example/demo/data/OrderFromCSV.java
@@ -0,0 +1,49 @@
+package com.example.demo.data;
+
+public class OrderFromCSV {
+    private String userName;
+    private String itemName;
+    private int amount;
+    private double purchasePrice;
+    private double sellPrice;
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public void setSellPrice(double sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
+}
