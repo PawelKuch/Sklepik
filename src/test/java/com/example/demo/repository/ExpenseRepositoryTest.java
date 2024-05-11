bb64c20364cba1949dd105f3fa06dae8381331f4
PawelKuch <kucharski.pawel99@gmail.com>
2024-05-12 00:35:52 +0200

    sklepik

--- src/test/java/com/example/demo/repository/ExpenseRepositoryTest.java
+++ src/test/java/com/example/demo/repository/ExpenseRepositoryTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -23,37 +24,60 @@ public class ExpenseRepositoryTest {
     @Autowired
     UserRepository userRepository;
     Expense expense;
+    List<Expense> expenses;
     User user;
+    User user2;
     @BeforeEach
     public void saveExpense(){
-        expense = new Expense();
         user = new User();
         user.setName("testUser");
         user.setUserId(UUID.randomUUID().toString());
         userRepository.save(user);
-        expense.setUser(user);
-        expense.setExpenseId(UUID.randomUUID().toString());
-        expense.setItem("testItem");
-        expense.setExpensePrice(0.1);
-        expense.setAmount(5);
-        expense.setTotalExpenseValue(expense.getAmount()*expense.getExpensePrice());
-        expense.setExpenseDateTime(LocalDateTime.now());
-        expenseRepository.save(expense);
 
+        user2 = new User();
+        user2.setName("testUser2");
+        user2.setUserId(UUID.randomUUID().toString());
+        userRepository.save(user2);
+
+        expenses = new ArrayList<>();
+        for (int i = 0; i < 5 ;i++){
+            expense = new Expense();
+            expense.setUser(user);
+            expense.setExpenseId(UUID.randomUUID().toString());
+            expense.setItem("testItem");
+            expense.setExpensePrice(0.1);
+            expense.setAmount(5);
+            expense.setTotalExpenseValue(expense.getAmount()*expense.getExpensePrice());
+            expense.setExpenseDateTime(LocalDateTime.now());
+            expenseRepository.save(expense);
+            expenses.add(expense);
+        }
     }
 
     @Test
     public void getTotalPurchaseValueForUserTest(){
         Optional<Double> totalPurchaseValueForUser = expenseRepository.getTotalPurchaseValueOfUser(user);
-        Double expectedValue = expense.getTotalExpenseValue();
+        Double expectedValue = getTotalExpensePurchaseValue(expenses);
         assertTrue(totalPurchaseValueForUser.isPresent());
         assertNotNull(totalPurchaseValueForUser);
         assertEquals(expectedValue, totalPurchaseValueForUser.get());
     }
+
+    public Double getTotalExpensePurchaseValue(List<Expense> expenses){
+        return expenses.stream().mapToDouble(Expense::getTotalExpenseValue)
+                .sum();
+    }
+
+    @Test
+    public void getTotalPurchaseValueForUser2Test(){
+        Optional<Double> totalPurchaseValueForUser = expenseRepository.getTotalPurchaseValueOfUser(user2);
+        assertFalse(totalPurchaseValueForUser.isPresent());
+    }
+
     @Test
     public void getTotalPurchaseValueTest(){
         Optional<Double> totalPurchaseValue = expenseRepository.getTotalPurchaseValue();
-        Double expectedValue = expense.getTotalExpenseValue();
+        Double expectedValue = getTotalExpensePurchaseValue(expenses);
         assertTrue(totalPurchaseValue.isPresent());
         assertNotNull(totalPurchaseValue);
         assertEquals(expectedValue, totalPurchaseValue.get());
@@ -64,17 +88,17 @@ public class ExpenseRepositoryTest {
         assertFalse(userExpensesStatistics.isEmpty());
         assertEquals(user.getUserId(), userExpensesStatistics.get(0).getUserId());
         assertEquals(user.getName(), userExpensesStatistics.get(0).getUserName());
-        Double expectedTotalValue = expense.getTotalExpenseValue();
+        Double expectedTotalValue = getTotalExpensePurchaseValue(expenses);
         assertEquals(expectedTotalValue, userExpensesStatistics.get(0).getTotalExpenseValue());
-        assertEquals(1, userExpensesStatistics.get(0).getHowManyExpenses());
+        assertEquals(expenses.size(), userExpensesStatistics.get(0).getHowManyExpenses());
     }
 
     @Test
     public void getExpenseDataQueryTest(){
         ExpenseDataQuery expenseDataQuery = expenseRepository.getExpenseDataQuery();
         assertNotNull(expenseDataQuery);
-        assertEquals(expense.getTotalExpenseValue(), expenseDataQuery.getTotalExpensesValue());
-        assertEquals(1, expenseDataQuery.getHowManyExpenses());
+        assertEquals(getTotalExpensePurchaseValue(expenses), expenseDataQuery.getTotalExpensesValue());
+        assertEquals(expenses.size(), expenseDataQuery.getHowManyExpenses());
     }
     @AfterEach
     public void deleteExpense(){
