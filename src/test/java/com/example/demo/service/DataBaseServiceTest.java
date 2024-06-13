cb51c3c3efc194e6a6aede6e6748bce283c63dcd
PawelKuch <kucharski.pawel99@gmail.com>
2024-06-13 23:34:44 +0200

    sklepik

--- src/test/java/com/example/demo/service/DataBaseServiceTest.java
+++ src/test/java/com/example/demo/service/DataBaseServiceTest.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import com.example.demo.data.OrderData;
+import com.example.demo.data.UserData;
 import com.example.demo.entity.Item;
 import com.example.demo.entity.Order;
 import com.example.demo.entity.User;
@@ -24,6 +26,8 @@ public class DataBaseServiceTest {
     OrderRepository orderRepository;
     @Mock
     ItemRepository itemRepository;
+    @Mock
+    ToDataService toDataService;
     @BeforeEach
     public void setUp(){
         MockitoAnnotations.openMocks(this);
@@ -108,4 +112,108 @@ public class DataBaseServiceTest {
         assertEquals(2.0, order1.getSellPrice());
     }
 
+    @Test
+    public void settleTheOrderTest(){
+        Order order1 = new Order();
+        order1.setOrderId("order1");
+        assertFalse(order1.isSettled());
+
+        Mockito.when(orderRepository.findByOrderId("order1")).thenReturn(order1);
+        dataBaseService.settleTheOrder("order1");
+        assertTrue(order1.isSettled());
+    }
+
+    @Test
+    public void removeItemsTest(){
+        List<String> itemsIds = Arrays.asList("item1", "item2");
+        Item item1 = new Item();
+        item1.setItemId("item1");
+
+        Item item2 = new Item();
+        item2.setItemId("item2");
+
+        Mockito.when(itemRepository.findByItemId("item1")).thenReturn(item1);
+        Mockito.when(itemRepository.findByItemId("item2")).thenReturn(item2);
+
+        ArgumentCaptor<Item> itemCaptor = ArgumentCaptor.forClass(Item.class);
+        dataBaseService.removeItems(itemsIds);
+        Mockito.verify(itemRepository, Mockito.times(2)).delete(itemCaptor.capture());
+        List<Item> capturedItems = itemCaptor.getAllValues();
+        assertEquals("item1", capturedItems.get(0).getItemId());
+        assertEquals("item2", capturedItems.get(1).getItemId());
+    }
+
+    @Test
+    public void getOrderTest(){
+        Order order1 = new Order();
+        order1.setOrderId("order1");
+
+        OrderData orderData = new OrderData();
+        orderData.setOrderId("order1");
+
+        Mockito.when(orderRepository.findByOrderId("order1")).thenReturn(order1);
+        Mockito.when(toDataService.convert(order1)).thenReturn(orderData);
+
+        OrderData orderDataResult = dataBaseService.getOrder("order1");
+        Mockito.verify(orderRepository).findByOrderId("order1");
+        Mockito.verify(toDataService).convert(order1);
+
+        assertNotNull(orderDataResult);
+        assertEquals("order1", orderDataResult.getOrderId());
+    }
+    @Test
+    public void getUserByNameTest(){
+        User user1 = new User();
+        user1.setName("user1");
+
+        Mockito.when(userRepository.findByName("user1")).thenReturn(user1);
+        User resultUser = dataBaseService.getUserByName("user1");
+        Mockito.verify(userRepository).findByName("user1");
+        assertNotNull(resultUser);
+        assertEquals("user1", resultUser.getName());
+    }
+
+    @Test
+    public void getItemByNameTest(){
+        Item item1 = new Item();
+        item1.setName("item1");
+
+        Mockito.when(itemRepository.findByName("item1")).thenReturn(item1);
+        Item itemResult = dataBaseService.getItemByName("item1");
+
+        Mockito.verify(itemRepository).findByName("item1");
+        assertNotNull(itemResult);
+        assertEquals("item1", itemResult.getName());
+    }
+
+    @Test
+    public void getUsersTest(){
+        User user1 = new User();
+        user1.setUserId("user1");
+
+        User user2 = new User();
+        user2.setUserId("user2");
+
+        List<User> users = Arrays.asList(user1, user2);
+
+
+        UserData userData1 = new UserData();
+        userData1.setUserId("user1");
+
+        UserData userData2 = new UserData();
+        userData2.setUserId("user2");
+
+        List<UserData> userDataList = Arrays.asList(userData1, userData2);
+
+        Mockito.verify(userRepository).findAll();
+        Mockito.verify(toDataService).getUsers(users);
+
+        Mockito.when(userRepository.findAll()).thenReturn(users);
+        Mockito.when(toDataService.getUsers(users)).thenReturn(userDataList);
+
+        List<UserData> listResult = dataBaseService.getUsers();
+        assertNotNull(listResult);
+        assertEquals("user1", listResult.get(0).getUserId());
+        assertEquals("user2", listResult.get(1).getUserId());
+    }
 }
