0b77157b57fa5a202b79494c10de3532f611055a
PawelKuch <kucharski.pawel99@gmail.com>
2024-08-23 20:56:56 +0200

    sklepik

--- src/test/java/com/example/demo/service/ToDataServiceTest.java
+++ src/test/java/com/example/demo/service/ToDataServiceTest.java
@@ -11,8 +11,6 @@ import com.example.demo.entity.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.Assertions;
 import java.time.LocalDateTime;
