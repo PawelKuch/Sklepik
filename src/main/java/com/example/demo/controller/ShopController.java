547865b443ae34df453881033126ce09474160d9
PawelKuch <kucharski.pawel99@gmail.com>
2024-09-07 19:01:18 +0200

    sklepik

--- src/main/java/com/example/demo/controller/ShopController.java
+++ src/main/java/com/example/demo/controller/ShopController.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.exception.ItemNotFoundException;
 import com.example.demo.exception.OrderNotFoundException;
 import com.example.demo.exception.UserNotFoundException;
-import com.example.demo.service.DataBaseService;
+import com.example.demo.service.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +14,30 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
+
 @Controller
-public class OrdersController {
+public class ShopController {
     DataBaseService dataBaseService;
-    public OrdersController(DataBaseService dataBaseService){
+    ToDataService toDataService;
+    FileHandlerService fileHandlerService;
+    ExpensesBaseService expensesBaseService;
+    StatisticsService statisticsService;
+    public ShopController(DataBaseService dataBaseService, ToDataService toDataService,
+                          FileHandlerService fileHandlerService, ExpensesBaseService expensesBaseService,
+                          StatisticsService statisticsService){
         this.dataBaseService = dataBaseService;
+        this.toDataService = toDataService;
+        this.fileHandlerService = fileHandlerService;
+        this.expensesBaseService = expensesBaseService;
+        this.statisticsService = statisticsService;
+    }
+    @GetMapping("/home-page")
+    public String getShop(Model model){
+        model.addAttribute("users", dataBaseService.getUsers());
+        model.addAttribute("products", dataBaseService.getItems());
+        model.addAttribute("orders", dataBaseService.getOrders());
+        model.addAttribute("shopPage", true);
+        return "shop";
     }
 
     @PostMapping("/home-page")
